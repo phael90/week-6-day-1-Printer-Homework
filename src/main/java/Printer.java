3d04f0e264a8fdf5d9ba3f14a1e7f0ce99ba75ab
Raphael Ugha <raphaelugha@gmail.com>
2018-07-30 22:13:56 +0100

    Added and tested getter for sheetNumber

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,8 +1,12 @@
 public class Printer {
     private int sheetsNumber;
 
-
+//constructor
     public Printer(int sheetsNumber){
         this.sheetsNumber = sheetsNumber;
+    }
+
+    public int getSheetsNumber(){
+        return this.sheetsNumber;
     }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+    Printer printer;
+
+    @Before
+    public void before(){
+    printer = new Printer(100);
+    }
+
+    @Test
+    public void sheetCount(){
+        assertEquals(100, printer.getSheetsNumber() );
+    }
 }
